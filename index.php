86e2e9395f1f7f890a02975ff252c696639c590d
David H <50745202+DavidHDJ@users.noreply.github.com>
2021-02-20 14:31:59 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,53 @@
+<html><head>
+<script src="https://www.googletagservices.com/activeview/js/current/osd.js?cb=%2Fr20100101"></script><script src="https://partner.googleadservices.com/gampad/cookie.js?domain=bbg.hojczyk.tech&amp;callback=_gfp_s_&amp;client=ca-pub-3218919129426828&amp;cookie=ID%3Dc697173e0702d821-2233c8e3cea60094%3AT%3D1613823242%3ART%3D1613823242%3AS%3DALNI_MZ89yHulAX4HY2SkejUTZP_tN397w"></script><script src="https://pagead2.googlesyndication.com/pagead/js/r20210211/r20190131/show_ads_impl_with_ama_fy2019.js?client=ca-pub-3218919129426828&amp;plah=bbg.hojczyk.tech&amp;amaexp=1" id="google_shimpl"></script><script src="js/jquery-3.3.1.js"></script>
+<script async="" src="js/popper.min.js"></script>
+<script async="" src="js/bootstrapmin.js"></script>
+<script async="" src="js/standart.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<script async="" src="https://www.googletagmanager.com/gtag/js?id=G-QGH4ZXEC3Z"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QGH4ZXEC3Z');
+</script>
+<script data-ad-client="ca-pub-3218919129426828" async="" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" data-checked-head="true"></script>
+<link media="all" rel="stylesheet" href="css/bootstrapmin.css">
+<link href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" rel="stylesheet">
+<link href="css/standart.css" rel="stylesheet">
+<meta http-equiv="origin-trial" content="A9ZgbRtm4pU3oZiuNzOsKcC8ppFSZdcjP2qYcdQrFKVzkmiWH1kdYY1Mi9x7G8+PS8HV9Ha9Cz0gaMdKsiVZIgMAAAB7eyJvcmlnaW4iOiJodHRwczovL2RvdWJsZWNsaWNrLm5ldDo0NDMiLCJmZWF0dXJlIjoiVHJ1c3RUb2tlbnMiLCJleHBpcnkiOjE2MjYyMjA3OTksImlzU3ViZG9tYWluIjp0cnVlLCJpc1RoaXJkUGFydHkiOnRydWV9"><meta http-equiv="origin-trial" content="A3HucHUo1oW9s+9kIKz8mLkbcmdaj5lxt3eiIMp1Nh49dkkBlg1Fhg4Fd/r0vL69mRRA36YutI9P/lJUfL8csQoAAACFeyJvcmlnaW4iOiJodHRwczovL2RvdWJsZWNsaWNrLm5ldDo0NDMiLCJmZWF0dXJlIjoiQ29udmVyc2lvbk1lYXN1cmVtZW50IiwiZXhwaXJ5IjoxNjI2MjIwNzk5LCJpc1N1YmRvbWFpbiI6dHJ1ZSwiaXNUaGlyZFBhcnR5Ijp0cnVlfQ=="><link rel="preload" href="https://adservice.google.de/adsid/integrator.js?domain=bbg.hojczyk.tech" as="script"><script type="text/javascript" src="https://adservice.google.de/adsid/integrator.js?domain=bbg.hojczyk.tech"></script><link rel="preload" href="https://adservice.google.com/adsid/integrator.js?domain=bbg.hojczyk.tech" as="script"><script type="text/javascript" src="https://adservice.google.com/adsid/integrator.js?domain=bbg.hojczyk.tech"></script></head>
+<body class="text-center">
+<video loading="lazy" autoplay="" muted="" loop="" style="    object-fit: cover;
+    position: absolute;
+    width: 100%;
+    left: 0;
+    z-index: -1;
+    top: 0;
+    height: 100%;">
+<source src="assets/background.mp4" type="video/mp4">
+</video>
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column" style="
+    background: #ffffffa1;
+">
+<header class="masthead mb-auto">
+<div class="inner">
+<h3 class="masthead-brand">Badminton</h3>
+
+</div>
+</header>
+<main role="main" class="inner cover">
+<h1 class="cover-heading">Willkommen</h1>
+<p class="lead">Diese Webseite dient für den Sportunterricht als Präsentation und ist in Kürze erreichbar</p>
+<p class="lead">
+</p>
+</main>
+<footer class="mastfoot mt-auto">
+<div class="inner">
+<p><a target="_blank" href="https://davids-appstudio.tk/impressum.php">Impressum</a> <a target="_blank" href="https://davids-appstudio.tk/Datenschutz.php">Datenschutzerklärung</a></p>
+</div>
+</footer>
+</div>
+</body>
+</html>
